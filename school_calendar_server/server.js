7b88ea1a36afa5efc2699a4b1c5fb4bb25bbc3fb
sukhveer12 <sukhveer.sahota@mail.utoronto.ca>
2020-06-06 10:38:07 -0400

    Added path for calendar API.

--- school_calendar_server/server.js
+++ school_calendar_server/server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-// Web API:
-var calendar_api = require('calendar_api.js');
-app.use(calendar_api.router);
+// Web API at root /calendar_api
+var calendar_api = require('./calendar_api.js');
+app.use("/calendar_api", calendar_api.router);
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
